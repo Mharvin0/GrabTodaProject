7e1205ed87aa22c36463f447e2236aa03dc76df0
John Radly Fernandez <jogu.fernandez.up@gmail.com>
2024-02-27 17:28:19 +0800

    update

--- app/src/main/java/com/example/todaproj/Dashboard.kt
+++ app/src/main/java/com/example/todaproj/Dashboard.kt
@@ -1,30 +1,21 @@
-package com.example.todaproj;
+package com.example.todaproj
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-
-public class Dashboard extends AppCompatActivity {
-
-    CardView BookingCard;
-    CardView FarecalcCard;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-
-        BookingCard = findViewById(R.id.BookingCard);
-
-        BookingCard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Dashboard.this, Booking.class);
-                startActivity(intent);
-            }
-        });
+class Dashboard : AppCompatActivity() {
+    var BookingCard: CardView? = null
+    var FarecalcCard: CardView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        BookingCard = findViewById(R.id.BookingCard)
+        BookingCard!!.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this@Dashboard, Booking::class.java)
+            startActivity(intent)
+        })
     }
 }
